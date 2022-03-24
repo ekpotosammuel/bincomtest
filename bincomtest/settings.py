913757a88fc5d08ae2906a678474beebf7deaf5e
samuel <ekpotosammuel1@gmail.com>
2022-03-23 18:35:43 -0700

    make it better

--- bincomtest/settings.py
+++ bincomtest/settings.py
@@ -83,10 +83,10 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dcfijp8qatg0qi', 
-        'USER': 'oicjujtdnitrlq', 
-        'PASSWORD': '67642f705244c4016c229a56f066963be595094b8b9bb8a58d0b907448068beb',
-        'HOST': 'ec2-18-215-96-22.compute-1.amazonaws.com', 
+        'NAME': 'bincom', 
+        'USER': 'postgres', 
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
 }
@@ -132,3 +132,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
